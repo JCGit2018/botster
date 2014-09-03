d881bc84c9eaf1ab96d60c338c716352f7d59963
Alex Lenton <lenton64@gmail.com>
2014-09-03 22:51:54 +0100

    Remove use of Botster::say() method

--- ajax/think.php
+++ ajax/think.php
@@ -20,12 +20,5 @@ if(!isset($_SESSION['conversation_id']))
 //Set conversation object
 $conversation = new Lenton\Botster\Conversation($_SESSION['conversation_id']);
 
-// Get Botster's response to conversation
-$response = $botster->respond($conversation);
-
-// If Botster made a response
-if ($response !== false)
-{
-	//Say output
-	$botster->say($response);
-}
+// Let Botster respond in conversation
+$botster->respond($conversation);
