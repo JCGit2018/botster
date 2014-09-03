092070777af06e568dc83cca60d3486057b5fe48
Alex Lenton <lenton64@gmail.com>
2014-09-03 15:48:51 +0100

    Revert "Fix untracked files"
    This reverts commit dac9cb19b4ad72b695c4b77f13be3ab3315ec9bd.

---
+++ settings.php
@@ -0,0 +1,14 @@
+<?php
+
+$settings = [
+	// Root of the domain with trailing slash
+	'domain_root' => '127.0.0.1/',
+	
+	// MySQL settings
+	'mysql' => [
+		'host' => '127.0.0.1',
+		'username' => 'root',
+		'password' => '',
+		'database' => 'botster',
+	],
+];
