67867b844e6b33201e1ca64237ad8b3dbc638e35
Alex Lenton <lenton64@gmail.com>
2014-09-03 21:39:25 +0100

    Remove all logging

--- ajax/think.php
+++ ajax/think.php
@@ -2,14 +2,6 @@
 
 require_once '../bootstrap.php';
 
-function mtime()
-{
-	$exploded = explode(" ", microtime());
-	return $exploded[0] + $exploded[1];
-}
-
-$start = mtime();
-
 //Connect to database
 db_connect();
 
@@ -24,7 +16,6 @@ if(!isset($_SESSION['conversation_id']))
 {
 	exit("Error: Conversation ID is not set.");
 }
-//$botster->log('Conversation ID: '.$_SESSION['conversation_id']);
 
 //Set conversation object
 $botster->conversation = new Lenton\Botster\Conversation($_SESSION['conversation_id']);
@@ -32,16 +23,9 @@ $botster->conversation = new Lenton\Botster\Conversation($_SESSION['conversation
 //Get input
 if(($previous_message = $botster->conversation->getMessage(0)) === false || $previous_message['author'] != 1) exit();
 $input = $previous_message['message'];
-	
-$botster->log('Input: '.$input);
 
 // Get Botster's response for input
 $response = $botster->respond($input);
-
-$finish = mtime();
-
-$execution_time = $finish - $start;
-$botster->log('Script executed in ' . $execution_time . ' seconds.');
 
 //Say output
 $botster->say($response);
