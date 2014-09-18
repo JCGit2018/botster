e3196f691a196299024548e3bcf916892b1b02c1
Alex Lenton <lenton64@gmail.com>
2014-09-18 16:17:52 +0100

    Pass conversation ID instead of object to respond() method

--- ajax/think.php
+++ ajax/think.php
@@ -8,21 +8,14 @@ $dbh = db_connect();
 // Continue session
 session_start();
 
-// Instantiate botster factory
-$botster_factory = new Lenton\Botster\Factory\Botster($dbh);
-
-//Instantiate botster object
-$botster = $botster_factory->make();
-
 //Check if conversation ID is set
 if(!isset($_SESSION['conversation_id']))
 {
 	exit("Error: Conversation ID is not set.");
 }
 
-//Set conversation object
-$conversation = $botster_factory->makeConversation();
-$conversation->getConversation($_SESSION['conversation_id']);
+//Instantiate botster object
+$botster = (new Lenton\Botster\Factory\Botster($dbh))->make();
 
 // Let Botster respond in conversation
-$botster->respond($conversation);
+$botster->respond($_SESSION['conversation_id']);
