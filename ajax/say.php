7bdb7f4ee71ff6271e44c26bad9bd50d7ee50e21
Alex Lenton <lenton64@gmail.com>
2014-09-22 13:34:51 +0100

    Remove use of conversation class

--- ajax/say.php
+++ ajax/say.php
@@ -2,34 +2,38 @@
 
 require_once '../bootstrap.php';
 
-//if(isset($_GET['input'])) $_POST['input'] = $_GET['input'];
-if(isset($_POST['input']))
+// If input is set
+if (isset($_POST['input']))
 {
-	//Connect to database
+	// Get input
+	$input = $_POST['input'];
+	
+	// Connect to database
 	$dbh = db_connect();
 	
 	// Continue session
 	session_start();
 	
-	$botster_factory = new Lenton\Botster\Factory\Botster($dbh);
-	$conversation = $botster_factory->makeConversation();
-	$botster = $botster_factory->make();
+	// Create Botster object
+	$botster = (new Lenton\Botster\Factory\Botster($dbh))->make();
 	
-	//If user isn't already in a conversation
-	if(!isset($_SESSION["conversation_id"]))
+	// If user is already in a conversation
+	if (isset($_SESSION["conversation_id"]))
 	{
-		//Get user information
-		$ip = $_SERVER['REMOTE_ADDR'];
-		$user_agent = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '';
-		
-		$conversation_id = $conversation->newConversation($ip, $user_agent);
-		$_SESSION['conversation_id'] = $conversation_id;
+		// Get conversation ID
+		$conversation_id = $_SESSION['conversation_id'];
 	}
 	else
 	{
-		$conversation->getConversation($_SESSION['conversation_id']);
+		// Get user information
+		$ip = $_SERVER['REMOTE_ADDR'];
+		$user_agent = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '';
+		
+		// Start new conversation
+		$conversation_id = $botster->newConversation($ip, $user_agent);
+		$_SESSION['conversation_id'] = $conversation_id;
 	}
 
-	//Add input to the conversation
-	$botster->say($conversation->id, 1, $_POST['input']);
+	// Say input in the conversation
+	$botster->say($conversation_id, 1, $input);
 }
