62950205041612d8639e223bd211ee3321e85a57
Alex Lenton <lenton64@gmail.com>
2014-10-09 09:34:46 +0100

    Make AJAX efficient
    JSON is now used instead of retrieving all of the data again as HTML. jQuery has been used in place of JavaScript for its ease of use. Mustache templates have also been used to avoid having HTML scattered throughout JavaScript.

--- ajax/log.php
+++ ajax/log.php
@@ -3,37 +3,48 @@
 require_once '../bootstrap.php';
 
 use Lentech\Botster\Factory;
-use Lentech\Botster\Entity\Message;
 
 // Continue session
 session_start();
 
-// If conversation ID is set
-if (isset($_SESSION['conversation_id']))
+// If conversation ID is not set
+if (! isset($_SESSION['conversation_id']))
 {
-	// Get conversation ID
-	$conversation_id = $_SESSION['conversation_id'];
-	
-	// Get database handler
-	$dbh = db_connect();
-	
-	// Instantiate message repository
-	$message_repository = (new Factory\Repository($dbh))->makeMessage();
-	
-	// Get conversation messages
-	$messages = array_reverse($message_repository->getLatestInConversation($conversation_id));
-	
-	// Output conversation log
-	foreach ($messages as $message)
-	{
-		// If author is user
-		if ($message->author == Message::USER)
-		{
-			echo '<span class="author">YOU:</span> '.htmlentities($message->message).'<br />';
-		}
-		else
-		{
-			echo '<b><span class="author">BOT:</span> '.htmlentities($message->message).'</b><br />';
-		}
-	}
+	return false;
 }
+
+// Get conversation ID
+$conversation_id = $_SESSION['conversation_id'];
+
+// Get database handler
+$dbh = db_connect();
+
+// Instantiate message repository
+$message_repository = (new Factory\Repository($dbh))->makeMessage();
+
+// Get last message ID is not set
+if (! isset($_GET['last']))
+{
+	// Get all conversation messages
+	$messages = array_reverse($message_repository->getLatestInConversation($conversation_id));
+}
+else
+{
+	// Get all in conversation after the message
+	$messages = $message_repository->getInConversationAfterMessage($conversation_id, $_GET['last']);
+}
+
+// Create JSON data array
+$json['messages'] = [];
+
+foreach ($messages as $message)
+{
+	$json['messages'][] = [
+		'id' => (int) $message->id,
+		'author' => (int) $message->author,
+		'message' => $message->message,
+	];
+}
+
+// Output JSON
+echo json_encode($json);
