20e2d9e4282f37aeb7a4ebe88326466a5a288e5b
Alex Lenton <lenton64@gmail.com>
2014-11-07 19:25:44 +0000

    Update favicon

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, user-scalable=no">
 		<meta name="description" content="Botster is an online chatbot artificial intelligence which learns from the conversations it has with users on the Internet." />
-		<link rel="shortcut icon" href="<?=DOMAIN_ROOT?>images/icon.png" />
+		<link rel="shortcut icon" href="<?=DOMAIN_ROOT?>images/logo_16.png" />
 		<link rel="stylesheet" href="<?=DOMAIN_ROOT?>css/style.css" />
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 		<script type="text/javascript" src="<?=DOMAIN_ROOT?>mustache/mustache.js"></script>
