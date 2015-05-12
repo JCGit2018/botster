151f5dd80ca1d4cb8b58ba4f24df950462287997
Alex Lenton <lenton64@gmail.com>
2015-05-12 11:47:54 +0100

    Change homepage title slogan

--- application/classes/Controller/Chat.php
+++ application/classes/Controller/Chat.php
@@ -4,7 +4,7 @@ class Controller_Chat extends Controller_Base
 {
 	public function action_index()
 	{
-        $this->template->title = 'Botster - Chatbot Artificial Intelligence';
+        $this->template->title = 'Botster - The friendly, fun, quirky chatbot';
         $this->page->set_filename('chat');
 	}
 }
